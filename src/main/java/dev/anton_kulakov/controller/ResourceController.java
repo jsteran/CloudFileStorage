ffa3c8f7b3ba78edd1e26fea1c0b950236e608ef
anton kulakov <kulakov-akv@yandex.ru>
2025-04-14 22:01:02 +0300

    fix: fix a problem with uploading files

--- src/main/java/dev/anton_kulakov/controller/ResourceController.java
+++ src/main/java/dev/anton_kulakov/controller/ResourceController.java
@@ -2,11 +2,14 @@ package dev.anton_kulakov.controller;
 
 import dev.anton_kulakov.dto.ResourceInfoDto;
 import dev.anton_kulakov.dto.StreamingResponseFactory;
+import dev.anton_kulakov.model.SecurityUser;
 import dev.anton_kulakov.service.FileService;
+import dev.anton_kulakov.service.PathHelper;
 import dev.anton_kulakov.service.ResourceServiceFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
@@ -19,6 +22,7 @@ public class ResourceController {
     private final ResourceServiceFactory resourceServiceFactory;
     private final StreamingResponseFactory streamingResponseFactory;
     private final FileService fileService;
+    private final PathHelper pathHelper;
 
     @GetMapping("/api/resource")
     public ResponseEntity<ResourceInfoDto> getInfo(@RequestParam String path) {
@@ -34,8 +38,10 @@ public class ResourceController {
     }
 
     @DeleteMapping("/api/resource")
-    public ResponseEntity<Void> delete(@RequestParam String path) {
-        resourceServiceFactory.getService(path).delete(path);
+    public ResponseEntity<Void> delete(@AuthenticationPrincipal SecurityUser securityUser,
+                                       @RequestParam String path) {
+        String userRootFolder = pathHelper.getUserRootFolder(securityUser.getUserId());
+        resourceServiceFactory.getService(path).delete(userRootFolder + path);
         return ResponseEntity.noContent().build();
     }
 
@@ -53,9 +59,12 @@ public class ResourceController {
     }
 
     @PostMapping("/api/resource")
-    public ResponseEntity<ResourceInfoDto> upload(@RequestParam String path, @RequestParam MultipartFile file) {
-        fileService.upload(path, file);
-        String newPath = path + file.getOriginalFilename();
+    public ResponseEntity<ResourceInfoDto> upload(@AuthenticationPrincipal SecurityUser securityUser,
+                                                  @RequestParam String path,
+                                                  @RequestParam MultipartFile object) {
+        String userRootFolder = pathHelper.getUserRootFolder(securityUser.getUserId());
+        fileService.upload(userRootFolder + path, object);
+        String newPath = userRootFolder + path + object.getOriginalFilename();
         ResourceInfoDto resourceInfoDto = resourceServiceFactory.getService(newPath).getInfo(newPath);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
