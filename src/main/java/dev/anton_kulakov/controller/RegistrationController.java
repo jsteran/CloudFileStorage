cde5a2cd8bbab1759a27015c5586884c3adda57a
anton kulakov <kulakov-akv@yandex.ru>
2025-05-12 08:15:10 +0300

    chore: move endpoints /api/auth/sign-up and /api/user/me to separate controllers

---
+++ src/main/java/dev/anton_kulakov/controller/RegistrationController.java
@@ -0,0 +1,56 @@
+package dev.anton_kulakov.controller;
+
+import dev.anton_kulakov.config.OpenApiConfig;
+import dev.anton_kulakov.dto.UserRequestDto;
+import dev.anton_kulakov.dto.UserResponseDto;
+import dev.anton_kulakov.mapper.UserMapper;
+import dev.anton_kulakov.model.User;
+import dev.anton_kulakov.service.MinioService;
+import dev.anton_kulakov.service.UserDetailsServiceImpl;
+import dev.anton_kulakov.service.UserService;
+import dev.anton_kulakov.util.PathProcessor;
+import dev.anton_kulakov.util.SecurityContextUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = OpenApiConfig.REGISTRATION_TAG)
+public class RegistrationController {
+    private final UserService userService;
+    private final UserMapper userMapper;
+    private final MinioService minioService;
+    private final PathProcessor pathProcessor;
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
+    private final SecurityContextUtil securityContextUtil;
+    @Operation(summary = "Creating a new user")
+    @PostMapping("/api/auth/sign-up")
+    public ResponseEntity<UserResponseDto> create(@Valid @RequestBody UserRequestDto userRequestDto,
+                                                  HttpServletRequest req,
+                                                  HttpServletResponse resp) {
+        User user = userService.createUser(userRequestDto);
+        UserResponseDto userResponseDto = userMapper.toResponseDto(user);
+        String userRootFolder = pathProcessor.getUserRootFolder(user.getId());
+        minioService.createEmptyFolder(userRootFolder);
+
+        UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(user.getUsername());
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        securityContextUtil.setAuthentication(authentication, req, resp);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(userResponseDto);
+    }
+}
