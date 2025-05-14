d23f2f4f852ff3f7e72466968ccd6f37bb6c4864
anton kulakov <kulakov-akv@yandex.ru>
2025-05-14 21:01:53 +0300

    chore: add ApiResponses to the UserController

--- src/main/java/dev/anton_kulakov/controller/UserController.java
+++ src/main/java/dev/anton_kulakov/controller/UserController.java
@@ -1,8 +1,14 @@
 package dev.anton_kulakov.controller;
 
 import dev.anton_kulakov.config.OpenApiConfig;
+import dev.anton_kulakov.dto.ErrorMessage;
 import dev.anton_kulakov.dto.UserResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +22,49 @@ import java.security.Principal;
 @Tag(name = OpenApiConfig.USER_TAG)
 public class UserController {
     @Operation(summary = "Getting the current user")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponseDto.class),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "Successful operation example",
+                                            value = "{\"username\":\"test_username\"}"
+                                    )
+                            }
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "User is not authenticated",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorMessage.class),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "Unauthorized user error",
+                                            value = "{\"message\":\"Invalid credentials\"}"
+                                    )
+                            }
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Unexpected error",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorMessage.class),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "Validation error example",
+                                            value = "{\"message\":\"We're sorry, but an unexpected error has occurred. Please try again later\"}"
+                                    )
+                            }
+                    ))
+    })
     @GetMapping("/api/user/me")
     public ResponseEntity<UserResponseDto> getUser(Principal principal) {
         return ResponseEntity.ok(new UserResponseDto(principal.getName()));
