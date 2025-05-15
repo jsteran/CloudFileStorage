87059668002274c5689c7c01e1d16934218af74a
anton kulakov <kulakov-akv@yandex.ru>
2025-05-15 19:53:17 +0300

    feat: add a method for testing when adding an existing user

--- src/test/java/dev/anton_kulakov/RegistrationControllerTest.java
+++ src/test/java/dev/anton_kulakov/RegistrationControllerTest.java
@@ -1,56 +1,80 @@
-//package dev.anton_kulakov;
-//
-//import lombok.SneakyThrows;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.DynamicPropertyRegistry;
-//import org.springframework.test.context.DynamicPropertySource;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.testcontainers.containers.PostgreSQLContainer;
-//import org.testcontainers.junit.jupiter.Container;
-//import org.testcontainers.junit.jupiter.Testcontainers;
-//import org.testcontainers.utility.DockerImageName;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@Testcontainers
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//public class RegistrationControllerTest {
-//
-//    @Autowired
-//    private MockMvc mvc;
-//
-//    @Container
-//    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
-//
-//    @DynamicPropertySource
-//    static void configureProperties(DynamicPropertyRegistry registry) {
-//        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-//        registry.add("spring.datasource.username", postgres::getUsername);
-//        registry.add("spring.datasource.password", postgres::getPassword);
-//    }
-//
-//    @Test
-//    @SneakyThrows
-//    void shouldCreateUser() {
-//
-//        String requestBody = """
-//                {
-//                  "username": "test_user1",
-//                  "password": "test_password"
-//                }
-//                """;
-//
-//        mvc.perform(post("http://localhost:8080/api/auth/sign-up")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(requestBody))
-//                .andExpect(status().isCreated())
-//                .andExpect(jsonPath("$.username").value("test_user1"));
-//    }
-//}
+package dev.anton_kulakov;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Testcontainers
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class RegistrationControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Container
+    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    @DisplayName("Creating a new user results in a new record in the database")
+    @SneakyThrows
+    void shouldCreateUser() {
+        String requestBody = """
+                {
+                  "username": "test_user1",
+                  "password": "test_password"
+                }
+                """;
+
+        mvc.perform(post("http://localhost:8080/api/auth/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.username").value("test_user1"));
+    }
+
+    @SneakyThrows
+    @Test
+    @DisplayName("Creating a user with a duplicate username results in an error")
+    void shouldThrowExceptionIfUserAlreadyExists() {
+        String requestBody = """
+                {
+                  "username": "test_user1",
+                  "password": "test_password"
+                }
+                """;
+
+        mvc.perform(post("http://localhost:8080/api/auth/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody));
+
+        mvc.perform(post("http://localhost:8080/api/auth/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isConflict());
+    }
+}
