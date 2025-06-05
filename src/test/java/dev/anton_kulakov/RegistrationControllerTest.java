dc01994a37efed58257acae3d18c5c58377115c0
anton kulakov <kulakov-akv@yandex.ru>
2025-06-05 11:24:14 +0300

    fix: fix a problem with storing sessions in redis for RegistrationControllerTest

--- src/test/java/dev/anton_kulakov/RegistrationControllerTest.java
+++ src/test/java/dev/anton_kulakov/RegistrationControllerTest.java
@@ -1,16 +1,21 @@
 package dev.anton_kulakov;
 
+import dev.anton_kulakov.service.MinioService;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -22,20 +27,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @Testcontainers
 @SpringBootTest
+@ImportAutoConfiguration(classes = {RedisAutoConfiguration.class})
 @AutoConfigureMockMvc
 @Transactional
 public class RegistrationControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockitoBean
+    private MinioService minioService;
+
     @Container
     private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+
+    @Container
+    private static final GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:latest"))
+            .withExposedPorts(6379);
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
+
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", () -> redis.getMappedPort(6379).toString());
     }
 
     @Test
