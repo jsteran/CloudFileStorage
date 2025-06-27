0ac6d0cf47da8838189784de80036b61566e91d8
anton kulakov <kulakov-akv@yandex.ru>
2025-06-27 10:18:19 +0300

    chore: change the id field from int to Integer in the User model

--- src/main/java/dev/anton_kulakov/model/User.java
+++ src/main/java/dev/anton_kulakov/model/User.java
@@ -15,7 +15,7 @@ import java.io.Serializable;
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String username;
     private String password;
 }
