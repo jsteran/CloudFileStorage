a56f3b6c044ca9a10dcb22a216980f6109807089
anton kulakov <kulakov-akv@yandex.ru>
2025-07-06 11:18:30 +0300

    refactor: use @ConfigurationProperties for Minio config

--- src/main/java/dev/anton_kulakov/config/MinioConfig.java
+++ src/main/java/dev/anton_kulakov/config/MinioConfig.java
@@ -1,19 +1,17 @@
 package dev.anton_kulakov.config;
 
 import io.minio.MinioClient;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationProperties(prefix = "minio")
+@Setter
 public class MinioConfig {
-    @Value("${minio.endpoint}")
     private String endpoint;
-
-    @Value("${minio.access-key}")
     private String accessKey;
-
-    @Value("${minio.secret-key}")
     private String secretKey;
 
     @Bean
