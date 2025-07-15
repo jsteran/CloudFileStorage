35125f8922625c3cef967028258d4fd4c3871986
anton kulakov <kulakov-akv@yandex.ru>
2025-07-15 10:45:11 +0300

    fix: ignore the id field when mapping

--- src/main/java/dev/anton_kulakov/mapper/UserMapper.java
+++ src/main/java/dev/anton_kulakov/mapper/UserMapper.java
@@ -4,9 +4,11 @@ import dev.anton_kulakov.dto.UserRequestDto;
 import dev.anton_kulakov.dto.UserResponseDto;
 import dev.anton_kulakov.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "id", ignore = true)
     User toUser(UserRequestDto userRequestDto);
     UserResponseDto toResponseDto(User user);
 }
